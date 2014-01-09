100d5d5463ba7c85b10169d5070dc309ee91b131
Ryan Petrello <ryan.petrello@dreamhost.com>
2014-01-09 10:34:49 -0800

    Automatically pull all submodule code.

--- dev/recipes/dotfiles.rb
+++ dev/recipes/dotfiles.rb
@@ -2,7 +2,7 @@ apt_package "git" do
   action :upgrade
 end
 
-execute "git init && git remote add origin https://github.com/ryanpetrello/dotfiles.git && git pull origin master" do
+execute "git init && git remote add origin https://github.com/ryanpetrello/dotfiles.git && git pull origin master && git submodule init && git submodule update" do
   cwd node[:devstack][:dir]
   user node[:devstack][:user]
   group node[:devstack][:group]
